282f301d52116d378283e24b7dfaff0f2561dd71
Rachmasari Annisa Rida <119955525+RachmasariAR@users.noreply.github.com>
2024-12-04 01:28:32 +0700

    Update server.py
    update perbaikan API

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 import uvicorn
-from fastapi import File, UploadFile
-from prediction import read_image
-from prediction import preprocess
-from prediction import predict
+from prediction import read_image, preprocess, predict
 
 app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the ML-Model API!"}
 
-@app.post('predict')
+@app.post("/predict")
 async def predict_image(file: UploadFile = File(...)):
-    image = read_image(await file)
+    image = read_image(await file.read())
     image = preprocess(image)
 
     predictions = predict(image)
     print(predictions)
-    return predictions
+    return {"predictions : ":predictions}
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host='0.0.0.0')
